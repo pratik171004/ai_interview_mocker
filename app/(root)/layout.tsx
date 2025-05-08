98a26b698d63e178bf3106a374ad648b5fbeda53
pratik171004 <pratikwaghmare546@gmail.com>
2025-05-08 11:21:00 +0530

    Created Home page User Interface , Cards

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,23 @@
 import React, { ReactNode } from 'react'
 import { Toaster } from 'sonner'
+import Link from 'next/link';
+import Image  from 'next/image';
+
+
 
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
-    <div>
-      {children}
-      <Toaster/>
-    </div>
+    <div className="root-layout">
+    <nav>
+      <Link href="/" className="flex items-center gap-2">
+        <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+        <h2 className="text-primary-100">PrepWise</h2>
+      </Link>
+    </nav>
+
+    {children}
+  </div>
+
   )
 }
 
